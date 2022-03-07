0584814df9f89b9af10bbf383daca9fccf2e84b7
R.Andres Castaneda <acastanedaa@worldbank.org>
2022-03-07 14:10:58 -0500

    init version in gist

---
+++ R/CHN_mean_ppp.R
@@ -0,0 +1,123 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# libraries   ---------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+library(data.table)
+library(ggplot2)
+
+options(pipload.verbose = FALSE)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Load data   ---------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+yrs   <- c(2016, 2017, 2018)
+yrs   <- c(1984:2019)
+
+# ld   <- pipload::pip_load_cache("CHN", yrs, type = "list")
+ld   <- pipload::pip_load_cache("CHN", type = "list")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## population --------
+
+pop <- pipload::pip_load_aux("pop")
+pop <- pop[country_code == "CHN"]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Group data means in LCU --------
+
+gdm <- pipload::pip_load_aux("gdm")
+means <-
+  gdm[country_code == "CHN"
+  ][,
+    mean := survey_mean_lcu* (12/365)
+  ][,
+    .(surveyid_year, pop_data_level, mean )]
+
+means <- split(means, by = "surveyid_year")
+
+means <-
+  purrr::map(.x = means,
+             .f = ~{
+               y <-       .x[, mean]
+               names(y) <- .x[, pop_data_level]
+               y
+             })
+
+years     <- names(means)
+names(ld) <- years
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Functions   ---------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+gmeans <- function(df, mn) {
+
+  levels <- df[, unique(as.character(reporting_level))]
+
+  y <- purrr::map(.x = levels,
+                  .f = ~{
+
+                    dfx <- df[reporting_level == .x]
+                    mnx <- mn[.x]
+
+                    cpi  <- dfx[, unique(cpi)]
+                    ppp  <- dfx[, unique(ppp)]
+
+                    mnx <- wbpip::deflate_welfare_mean(welfare_mean = mnx,
+                                                       ppp = ppp,
+                                                       cpi = cpi)
+
+                    return(mnx)
+                  })
+  return(y)
+}
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Execution   ---------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+dd <- purrr::map(.x = years,
+                 .f = ~{
+                   mn <- means[[.x]]
+                   df <- ld[[.x]]
+                   z <- gmeans(df, mn)
+                   # z <- copy(z)
+                   # z[, year := as.numeric(.x)]
+                   return(z)
+                 })
+
+
+ff <- as.data.table(dd)
+
+setnames(ff, names(ff), years)
+ff[,
+   area := c("rural", "urban")]
+
+ff <- melt(ff, id = "area",
+           variable.name = "year",
+           value.name    = "means",
+           variable.factor = FALSE)
+
+ff <-
+  ff[,
+     year := as.numeric(year)
+  ][year  >= 2011
+  ][, means := {
+    x <- as.numeric(means)
+    x <- x*(365/12)
+  }
+  ]
+
+
+ff <- dcast(ff, year ~ area, value.var = "means")
+
+
+gt_ff <- gt(ff)
+
+gt_ff %>%
+  tab_header(
+    title = "Monthly mean in 2011 PPPs"
+  )
---
+++ R/poverty_estimates.R
@@ -0,0 +1,173 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# libraries   ---------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+library(data.table)
+library(ggplot2)
+
+options(pipload.verbose = FALSE)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Load data   ---------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+yrs   <- c(2016, 2017, 2018)
+yrs   <- c(1984:2018)
+
+# ld   <- pipload::pip_load_cache("CHN", yrs, type = "list")
+ld   <- pipload::pip_load_cache("CHN", type = "list")
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## population --------
+
+pop <- pipload::pip_load_aux("pop")
+pop <- pop[country_code == "CHN"]
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Group data means in LCU --------
+
+gdm <- pipload::pip_load_aux("gdm")
+means <-
+  gdm[country_code == "CHN"
+  ][,
+    mean := survey_mean_lcu* (12/365)
+  ][,
+    .(surveyid_year, pop_data_level, mean )]
+
+means <- split(means, by = "surveyid_year")
+
+means <-
+  purrr::map(.x = means,
+             .f = ~{
+               y <-       .x[, mean]
+               names(y) <- .x[, pop_data_level]
+               y
+             })
+
+years     <- names(means)
+names(ld) <- years
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Functions   ---------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+gd_povstats <- function(df, mn) {
+
+  levels <- df[, unique(as.character(reporting_level))]
+
+  y <- purrr::map_df(.x = levels,
+                     .f = ~{
+
+                       dfx <- df[reporting_level == .x]
+                       mnx <- mn[.x]
+
+                       cpi  <- dfx[, unique(cpi)]
+                       ppp  <- dfx[, unique(ppp)]
+
+                       mnx <- wbpip::deflate_welfare_mean(welfare_mean = mnx,
+                                                          ppp = ppp,
+                                                          cpi = cpi)
+
+                       st <-
+                         purrr::map_df(
+                           .x = c(1.9, 3.2, 5.5),
+                           .f = ~{
+                             rs <- wbpip:::gd_compute_poverty_stats(welfare = dfx$welfare,
+                                                                    population = dfx$weight,
+                                                                    povline = .x,
+                                                                    requested_mean = mnx)
+
+                             rs <- as.data.table(rs)
+                           })
+                       st <- copy(st)
+
+                       st[, data_level := .x]
+                       return(st)
+                     })
+  return(y)
+}
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Execution   ---------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+dd <- purrr::map_df(.x = years,
+                    .f = ~{
+                      mn <- means[[.x]]
+                      df <- ld[[.x]]
+                      z <- gd_povstats(df, mn)
+                      z <- copy(z)
+                      z[, year := as.numeric(.x)]
+                      return(z)
+                    })
+
+df <- joyn::merge(dd, pop, by = c("year", "data_level = pop_data_level"),
+                  match_type = "m:1",
+                  keep = "left",
+                  reportvar = FALSE)
+
+dfn <- df[,
+          lapply(.SD, weighted.mean, w = pop),
+          .SDcols = c("headcount", "poverty_gap", "poverty_severity", "watts"),
+          by = c("year", "poverty_line")
+][,
+  data_level := "national"]
+
+
+vars <- names(dfn)
+dff = rbindlist(list(df[, ..vars], dfn), use.names = TRUE)
+
+setorder(dff, year, poverty_line, data_level)
+
+pcn <- purrr::map_df(.x = c(1.9, 3.2, 5.5),
+                     .f = ~{
+                       povcalnetR::povcalnet(country = "CHN",
+                                             povline = .x)
+                     })
+pcn <- as.data.table(pcn)
+
+
+tokeep <- c("datayear", "data_level", "povertyline", "headcount")
+
+pcn2 <-
+  pcn[,
+      data_level := fcase(coveragetype == "A", "national",
+                          coveragetype == "U", "urban",
+                          coveragetype == "R", "rural"
+      )
+  ][,
+    ..tokeep]
+
+dfc <- joyn::merge(dff, pcn2, by = c("year = datayear",
+                                     "data_level",
+                                     "poverty_line = povertyline"),
+                   match_type = "1:1",
+                   keep = "right",
+                   keep_y_in_x = TRUE)
+
+
+dfc[,
+    diff := abs(headcount - headcount.y)]
+
+collapse::qsu(dfc, diff ~ data_level+year)
+
+filename <- fs::path(tdirp, "CHN_pov", ext = "dta")
+haven::write_dta(dff,filename)
+
+
+# chart
+ggplot(dff[year >= 2000],
+       aes(x = year,
+           y = headcount,
+           color = as.factor(poverty_line))
+) +
+  geom_line() +
+  facet_wrap(~data_level) +
+  theme_minimal() +
+  theme(legend.title = element_blank(),
+        legend.position = "bottom")
+
+
